efb04430557940aebb77b269d51146533fd6bcfb
Cleiton carlos <cleitoncarlos.programmer@gmail.com>
2023-10-14 09:20:20 -0300

    script finalizado do index

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+import addClass from "./addClass.js";
+import removeClass from "./removeClass.js";
+
+const buttons = document.querySelectorAll('.services__buttonRead');
+const services = document.querySelectorAll('.services__readMore');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
+const newImage = document.createElement('img');
+
+buttons.forEach((button,index) => {
+  button.addEventListener('click',() => {
+    addClass(services[index]);
+    body.classList.add('scrollNone');
+    overlay.classList.add('block');
+  })
+})
+
+document.querySelectorAll('.content__image').forEach((image) => {
+  image.addEventListener('click',(event) => {
+    newImage.classList.add('activeImage');
+    newImage.src = event.target.src;
+    body.appendChild(newImage);
+    body.classList.add('scrollNone');
+    overlay.classList.add('block');
+  })
+})
+
+overlay.addEventListener('click', () => {
+  services.forEach((service) => {
+    removeClass(service,overlay,body);
+  })
+  body.removeChild(newImage);
+})
