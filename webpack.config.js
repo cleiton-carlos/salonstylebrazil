a955705c540dd9383039fa5bbaa66656e8323cbe
Cleiton carlos <cleitoncarlos.programmer@gmail.com>
2023-10-15 23:11:07 -0300

    adicionados mais alguns arquivos de entrada no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: ['./src/js/index.js','./src/js/menu.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
