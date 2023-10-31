65e030a7b73f078f7b28b53cf219296c7d6c2732
Cleiton carlos <cleitoncarlos.programmer@gmail.com>
2023-10-30 23:19:49 -0300

    corrijido alguns conflitos de arquivos

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -1,18 +1,3 @@
-import addClass from "./addClass";
-import removeClass from "./removeClass";
+import menuMobile from "./menuMobile";
 
-let navigation = document.querySelector('.navigation');
-let body = document.querySelector('body');
-let menuBack = document.querySelector('.menuBack');
-
-document.querySelector('.header__buttonMenu').addEventListener('click',() => {
-  addClass(navigation,'flex');
-  addClass(body,'scrollNone');
-  addClass(menuBack,'block');
-})
-
-menuBack.addEventListener('click',() => {
-  removeClass(menuBack);
-  removeClass(navigation);
-  removeClass(body);
-})
+menuMobile();
